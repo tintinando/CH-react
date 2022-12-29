5feb8dea6317c82a5bb569c91ded9f7b914c3237
Martin <tintinando@gmail.com>
2022-12-29 18:29:07 -0300

    Modal carrito

--- src/components/cartWidget/CartWidget.js
+++ src/components/cartWidget/CartWidget.js
@@ -2,18 +2,46 @@
 
 // ------------ IMPORT ------------
 import "./CartWidget.css"
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
-import { Badge } from "react-bootstrap";
+import { Badge, Button, Modal } from "react-bootstrap";
 
 // ------------ LOGIC -------------
 function CartWidget(props) {
-  console.log(props);
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-   <>
-      Ver Carrito <FontAwesomeIcon className="me-1" icon={faCartShopping} />
-      <Badge bg="secondary">{props.length}</Badge>
+    <>
+      <Button
+        variant="dark"
+        onClick={handleShow}
+      >Ver Carrito <FontAwesomeIcon className="me-1" icon={faCartShopping} />
+        <Badge bg="secondary">{props.length}</Badge>
+      </Button>
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        animation={false}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Finalizar compra</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="text-center">
+          <FontAwesomeIcon className="usr-modal--icon" icon={faCartShopping} />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   )
 }
